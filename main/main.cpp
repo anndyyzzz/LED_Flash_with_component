3b995c97ff54eac171995922b647df2a5c5924ce
anndyyz <anda2546r@gmail.com>
2024-08-01 16:20:07 +0700

    code3

--- main/main.cpp
+++ main/main.cpp
@@ -12,52 +12,16 @@ LED led6(21);
 LED led7(22); 
 LED led8(23); 
 
-void ALL_LED_OFF()
-{
-    led1.OFF();
-    led2.OFF();
-    led3.OFF();
-    led4.OFF();
-    led5.OFF();
-    led6.OFF();
-    led7.OFF();
-    led8.OFF();
-}
+LED leds[] = {led1, led2, led3, led4, led5, led6, led7, led8};
 
 extern "C" void app_main(void)
 {
     int i = 0;
     while(1)
     {        
-        switch(i)
-        {
-            case 0:
-            led1.ON();
-            break;
-            case 1:
-            led2.ON();
-            break;
-            case 2:
-            led3.ON();
-            break;
-            case 3:
-            led4.ON();
-            break;
-            case 4:
-            led5.ON();
-            break;
-            case 5:
-            led6.ON();
-            break;
-            case 6:
-            led7.ON();
-            break;
-            case 7:
-            led8.ON();
-            break;            
-        }
+        leds[i].ON();
         vTaskDelay(100/portTICK_PERIOD_MS);
-        ALL_LED_OFF();
+        leds[i].OFF();
         if(i++ >= 7) i = 0;
     }
-}   
+}
