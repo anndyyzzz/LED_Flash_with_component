00bd4b96cd5e937b3c7ed6ce274c3d86e17d287d
anndyyz <anda2546r@gmail.com>
2024-08-01 16:18:22 +0700

    code2

--- main/main.cpp
+++ main/main.cpp
@@ -3,19 +3,61 @@
 #include "freertos/task.h"
 #include "LED.h"
 
-extern "C" void app_main(void);
+LED led1(16); 
+LED led2(17); 
+LED led3(5); 
+LED led4(18); 
+LED led5(19); 
+LED led6(21); 
+LED led7(22); 
+LED led8(23); 
 
-void app_main(void)
+void ALL_LED_OFF()
 {
-    LED led1(5); 
-    LED led2(17); 
+    led1.OFF();
+    led2.OFF();
+    led3.OFF();
+    led4.OFF();
+    led5.OFF();
+    led6.OFF();
+    led7.OFF();
+    led8.OFF();
+}
+
+extern "C" void app_main(void)
+{
+    int i = 0;
     while(1)
     {        
-        led1.ON();
-        led2.OFF();
-        vTaskDelay(500/portTICK_PERIOD_MS);
-        led1.OFF();
-        led2.ON();
-        vTaskDelay(500/portTICK_PERIOD_MS);
+        switch(i)
+        {
+            case 0:
+            led1.ON();
+            break;
+            case 1:
+            led2.ON();
+            break;
+            case 2:
+            led3.ON();
+            break;
+            case 3:
+            led4.ON();
+            break;
+            case 4:
+            led5.ON();
+            break;
+            case 5:
+            led6.ON();
+            break;
+            case 6:
+            led7.ON();
+            break;
+            case 7:
+            led8.ON();
+            break;            
+        }
+        vTaskDelay(100/portTICK_PERIOD_MS);
+        ALL_LED_OFF();
+        if(i++ >= 7) i = 0;
     }
-}
+}   
